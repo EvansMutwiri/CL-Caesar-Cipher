f3450cef2a650b07d527871681012486c66962e8
EvansMutwiri <evans.mwiti@student.moringaschool.com>
2021-08-21 15:57:57 +0300

    main method in App

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,51 @@
+import java.util.Scanner;
+
 public class App {
-    
+
+    public static void main(String[] args){
+
+        boolean appRunning = true;
+
+        while (appRunning){
+
+            System.out.print("1. Encryption\n2. Decryption\nChoose(1,2 or 3 to exit): ");
+            Scanner in = new Scanner(System.in);
+            int choice = in.nextInt();
+
+            if (choice == 1){
+                System.out.println("Encryption");
+                in.nextLine();
+
+                System.out.print("Enter text to encrypt: ");
+                String text = in.nextLine();
+
+                System.out.print("Enter key: ");  //<26
+                int shift = in.nextInt();
+
+                Encrypt newEncrypt = new Encrypt(text, shift);
+
+                System.out.println("Success! \n Your encrypted message is :" + newEncrypt.Encrypt(text, shift));
+
+            } else if (choice == 2){
+                System.out.println("Decryption");
+                in.nextLine();
+
+                System.out.print("Enter message to be decrypted: ");
+                String text = in.nextLine();
+
+                System.out.println("Enter key: ");
+                int shift = in.nextInt();
+
+                Decrypt newDecrypt = new Decrypt(text,shift);
+
+                System.out.println("SUCCESS. \n Your encrypted message: " + newDecrypt.decryptMessage(text, shift));
+
+            } else if (choice == 3){
+                System.out.println("Exit");
+                appRunning=false;
+            }else {
+                System.out.println("Try again!");
+            }
+        }
+    }
 }
