1ddfa4e0ec9fbbacfa0e9b182a43fafe4dc0327f
EvansMutwiri <evans.mwiti@student.moringaschool.com>
2021-08-21 19:06:22 +0300

    encrypt tests

--- src/test/java/models/EncryptTest.java
+++ src/test/java/models/EncryptTest.java
@@ -5,26 +5,44 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class EncryptTest {
-    private Encrypt test = new Encrypt("abc", 2);
+
+    public void runEncrypt_instantiate(){
+        Encrypt test = new Encrypt("abc", 2);
+        assertTrue(test instanceof Encrypt);
+    }
 
     @Test
     void getText() {
-
+        Encrypt test = new Encrypt("abc", 2);
+        String testText = test.getText();
+        assertEquals("abc", testText);
     }
 
     @Test
     void setText() {
+        Encrypt test = new Encrypt("abc", 2);
+        test.setText("cbd");
+        String testText = test.getText();
+        assertEquals("cbd", testText);
     }
 
     @Test
     void getShift() {
+        Encrypt test = new Encrypt("abc", 2);
+        int testShift = test.getShift();
+        assertEquals(2, testShift);
     }
 
     @Test
-    void setShift() {
+    void setsShift() {
+        Encrypt test = new Encrypt("abc", 2);
+        test.setShift(3);
+        int testShift = test.getShift();
+        assertEquals(3, testShift);
     }
 
     @Test
     void encryptMessage() {
+        Encrypt test = new Encrypt("abc", 2);
     }
 }
